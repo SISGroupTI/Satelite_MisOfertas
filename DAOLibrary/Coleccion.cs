2963ecbfff98ad7c9f88f8f2c9c4a0a5340ff71b
Ian Cardenas <iancardenas96@gmail.com>
2017-09-11 19:43:29 -0300

    Se crea una coleccion de trabajadores

---
+++ DAOLibrary/Coleccion.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EntityLibrary;
+
+
+namespace DAOLibrary
+{
+    class Coleccion
+    {
+        private List<Trabajador> trabajadores;
+        private Trabajador encargado, administrador, gerente;
+
+        private Coleccion()
+        {
+            if (Trabajadores == null)
+            {
+                Trabajadores = new List<Trabajador>();
+                setTrabajadores();
+                trabajadores.Add(encargado);
+                trabajadores.Add(administrador);
+                trabajadores.Add(gerente);
+            }
+        }
+
+        public List<Trabajador> Trabajadores { get => trabajadores; set => trabajadores = value; }
+
+        private void setTrabajadores()
+        {
+            encargado = new Trabajador();
+            encargado.Nombre_usuario = "al.cardenas";
+            encargado.Contrasena = "lamoda";
+            encargado.Id_perfil = 3;
+            administrador = new Trabajador();
+            administrador.Nombre_usuario = "so.braumuller";
+            administrador.Contrasena = "lachora";
+            administrador.Id_perfil = 2;
+            gerente = new Trabajador();
+            gerente.Nombre_usuario = "ia.cardenas";
+            gerente.Contrasena = "4961.qwas";
+            gerente.Id_perfil = 1;
+        }
+    }
+}
---
+++ EntityLibrary/Trabajador.cs
@@ -0,0 +1,29 @@
+﻿namespace EntityLibrary
+{
+    public class Trabajador
+    {
+        private int _id_trabajador;
+        private int _id_perfil;
+        private int _id_local;
+        private string _nombre_usuario;
+        private int _rut;
+        private char[] _dv;
+        private string _nombre;
+        private string _apellidos;
+        private string _contrasena;
+
+        public int Id_trabajador { get => _id_trabajador; set => _id_trabajador = value; }
+        public int Id_perfil { get => _id_perfil; set => _id_perfil = value; }
+        public int Id_local { get => _id_local; set => _id_local = value; }
+        public string Nombre_usuario { get => _nombre_usuario; set => _nombre_usuario = value; }
+        public int Rut { get => _rut; set => _rut = value; }
+        public char[] Dv { get => _dv; set => _dv = value; }
+        public string Nombre { get => _nombre; set => _nombre = value; }
+        public string Apellidos { get => _apellidos; set => _apellidos = value; }
+        public string Contrasena { get => _contrasena; set => _contrasena = value; }
+
+        public Trabajador()
+        {
+        }
+    }
+}
