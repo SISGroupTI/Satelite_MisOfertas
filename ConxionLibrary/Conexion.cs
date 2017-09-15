fa7eaa3a4d11f2260830380e53c51802cd68a3b9
IanCardenas <iancardenas96@gmail.com>
2017-09-15 07:54:09 -0300

    No se logra la conexion con la base de datos

---
+++ ConxionLibrary/Conexion.cs
@@ -0,0 +1,38 @@
+﻿using Oracle.DataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConxionLibrary
+{
+    public class Conexion
+    {
+        OracleConnection con;
+
+        public Conexion()
+        {
+            if (con==null)
+            {
+                con = new OracleConnection();
+            }
+        }
+        public OracleConnection Connect()
+        {
+            con.ConnectionString = "User Id=<username>;Password=<password>;Data Source=<datasource>";
+            return con;
+        }
+
+        public void Close()
+        {
+            con.Close();
+            con.Dispose();
+        }
+
+        public void Open()
+        {
+            con.Open();
+        }
+        
+    }
+}
