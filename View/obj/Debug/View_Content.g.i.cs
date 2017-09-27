9cc6d72931f6136ca1377a0b17a44a55ee9e2755
ianCardenasCastillo <iancardenas96@gmail.com>
2017-09-26 22:54:18 -0300

    commit my changes

--- View/obj/Debug/View_Content.g.i.cs
+++ View/obj/Debug/View_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("img/empty.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("imagen-ms%20sinlogo.ico")]
 
 
